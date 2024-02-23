781f8b8cac79451f9cc7386459bdae43638324ca
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-02-23 00:16:30 -0300

    feat: Add status code to endpoints

--- src/server.js
+++ src/server.js
@@ -8,16 +8,30 @@ import http from 'node:http';
 // PATCH => Update data to the server; (When needs to update just once piece of data)
 // DELETE => Delete data from server;
 
+// Stateful - Stateless
+
+// Headers req/res => MetaData
+
+const users = [];
+
 const server = http.createServer((req, res) => {
     const { url, method } = req;
 
-    if(url === '/' && method === 'GET') return res.end('Hello, World!');
+    if(url === '/' && method === 'GET') return res.writeHead(200).end('Hello, World!');
 
-    if(url === '/users' && method === 'GET') return res.end('Users List!');
+    if(url === '/users' && method === 'GET') return res.writeHead(200).setHeader('Content-type', 'application/json').end(JSON.stringify(users));
 
-    if(url === '/users' && method === 'POST') return res.end('Create User!');
+    if(url === '/users' && method === 'POST') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+        })
 
-    return res.end('Not Found!');
+        return res.writeHead(201).end();
+    }
+
+    return res.writeHead(404).end();
 });
 
 server.listen(3333);
