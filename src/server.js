5404a75e6cfb5375266fd05534b778c5ddec7ac1
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-02-22 23:42:42 -0300

    feat: Add some endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 import http from 'node:http';
 
+// Common HTTP Methods: GET, POST, PUT, PATCH, DELETE
+
+// GET => Get some data from server;
+// POST => Post new data to the server;
+// PUT => Replace data to the server; (When needs to update a big piece of data)
+// PATCH => Update data to the server; (When needs to update just once piece of data)
+// DELETE => Delete data from server;
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello World!');
+    const { url, method } = req;
+
+    if(url === '/' && method === 'GET') return res.end('Hello, World!');
+
+    if(url === '/users' && method === 'GET') return res.end('Users List!');
+
+    if(url === '/users' && method === 'POST') return res.end('Create User!');
+
+    return res.end('Not Found!');
 });
 
 server.listen(3333);
