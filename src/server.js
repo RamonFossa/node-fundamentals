5ca541a413fa3ff48fded0740a59d4820cc52505
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-02-28 23:15:45 -0300

    feat: `/users` get paramethers from body

--- src/server.js
+++ src/server.js
@@ -14,21 +14,39 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { url, method } = req;
+    console.log('REQ => ', url, method);
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+        console.log(chunk);
+        console.log(chunk.toString());
+        buffers.push(chunk);
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        req.body = null;
+    }
+
+    console.log(req.body);
 
     if(url === '/' && method === 'GET') return res.writeHead(200).end('Hello, World!');
 
-    if(url === '/users' && method === 'GET') return res.writeHead(200).setHeader('Content-type', 'application/json').end(JSON.stringify(users));
+    if(url === '/users' && method === 'GET') return res.setHeader('Content-type', 'application/json').writeHead(200).end(JSON.stringify(users));
 
     if(url === '/users' && method === 'POST') {
-        users.push({
-            id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com',
-        })
+        const { name, email } = req.body;
 
-        return res.writeHead(201).end();
+        users.push({
+            name,
+            email,
+        });
+
+        return res.writeHead(201).end('User created successfully!');
     }
 
     return res.writeHead(404).end();
